3b265075bcd40a9d7a80ce8c5811515813bb5c27
Geoffrey Thompson <fleaplus@gmail.com>
2017-09-07 14:47:57 -0600

    fix preview orientation and slightly increase stream quality

--- app/src/main/java/com/newlinetechnicalinnovations/rtspserverpoc/MainActivity.java
+++ app/src/main/java/com/newlinetechnicalinnovations/rtspserverpoc/MainActivity.java
@@ -3,6 +3,7 @@ package com.newlinetechnicalinnovations.rtspserverpoc;
 import net.majorkernelpanic.streaming.SessionBuilder;
 import net.majorkernelpanic.streaming.gl.SurfaceView;
 import net.majorkernelpanic.streaming.rtsp.RtspServer;
+import net.majorkernelpanic.streaming.video.VideoQuality;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -19,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
         SessionBuilder.getInstance()
                 .setSurfaceView(surfaceView)
+                .setPreviewOrientation(90)
                 .setContext(getApplicationContext())
                 .setAudioEncoder(SessionBuilder.AUDIO_NONE)
-                .setVideoEncoder(SessionBuilder.VIDEO_H264);
+                .setVideoEncoder(SessionBuilder.VIDEO_H264)
+                .setVideoQuality(new VideoQuality(320,240,20,500000));
 
         this.startService(new Intent(this, RtspServer.class));
     }
