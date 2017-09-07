a3d997fb122e5f4619805d69502a66a89a7b02f5
Geoffrey Thompson <fleaplus@gmail.com>
2017-09-07 13:57:25 -0600

    add basic streaming setup

--- app/src/main/java/com/newlinetechnicalinnovations/rtspserverpoc/MainActivity.java
+++ app/src/main/java/com/newlinetechnicalinnovations/rtspserverpoc/MainActivity.java
@@ -1,5 +1,10 @@
 package com.newlinetechnicalinnovations.rtspserverpoc;
 
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        SessionBuilder.getInstance()
+                .setSurfaceView(surfaceView)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setVideoEncoder(SessionBuilder.VIDEO_H264);
+
+        this.startService(new Intent(this, RtspServer.class));
     }
 }
