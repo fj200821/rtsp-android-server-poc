088db1f568d9752b10b64dda3e568932817d0ed4
Geoffrey Thompson <fleaplus@gmail.com>
2017-09-08 08:47:22 -0600

    lock screen to landscape mode, adjust preview to not be stretched

--- app/src/main/java/com/newlinetechnicalinnovations/rtspserverpoc/MainActivity.java
+++ app/src/main/java/com/newlinetechnicalinnovations/rtspserverpoc/MainActivity.java
@@ -6,6 +6,7 @@ import net.majorkernelpanic.streaming.rtsp.RtspServer;
 import net.majorkernelpanic.streaming.video.VideoQuality;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,15 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        surfaceView.setAspectRatioMode(SurfaceView.ASPECT_RATIO_PREVIEW);
 
         SessionBuilder.getInstance()
                 .setSurfaceView(surfaceView)
-                .setPreviewOrientation(90)
                 .setContext(getApplicationContext())
                 .setAudioEncoder(SessionBuilder.AUDIO_NONE)
                 .setVideoEncoder(SessionBuilder.VIDEO_H264)
-                .setVideoQuality(new VideoQuality(320,240,20,500000));
+                .setVideoQuality(new VideoQuality(320,240,30,500000));
 
         this.startService(new Intent(this, RtspServer.class));
     }
